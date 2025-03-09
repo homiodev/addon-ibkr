7ca20ba9783e8cbb7e62f5b6e1e0a73f3ddd102a
Ruslan Masiuk <ruslan.masiuk@tsimagine.com>
2025-03-10 00:29:12 +0200

    update widget

--- src/main/java/org/homio/addon/ibkr/IBKREntrypoint.java
+++ src/main/java/org/homio/addon/ibkr/IBKREntrypoint.java
@@ -1,6 +1,7 @@
 package org.homio.addon.ibkr;
 
 import lombok.extern.log4j.Log4j2;
+import org.homio.api.AddonConfiguration;
 import org.homio.api.AddonEntrypoint;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import java.util.Objects;
 
 @Log4j2
 @Component
+@AddonConfiguration
 public class IBKREntrypoint implements AddonEntrypoint {
 
     @Override
